5e51abdaa125bd297194febc7aaac038859c4d7e
JoseMTetis <josemunozherrera99@gmail.com>
2023-03-16 09:13:08 +0100

    Commit numero 4 desde la rama cambios

--- hola-mundo/src/pruebas/RamaCambios2.java
+++ hola-mundo/src/pruebas/RamaCambios2.java
@@ -5,7 +5,7 @@ public class RamaCambios2 {
 	public static void main(String[] args) {
 		int numerogoofy=7;
 		System.out.println("Te enseño este maravilloso numero : "+numerogoofy);
-
+		System.out.println("Te enseño este maravilloso numero por segunda vez ijijijijij "+(numerogoofy+1));
 	}
 
 }
