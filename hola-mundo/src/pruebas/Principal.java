ee9c12648fdf01dcd9d7c6c52f8d64c96009d3df
JoseMTetis <josemunozherrera99@gmail.com>
2023-03-16 08:58:22 +0100

    commit 2 lets ggggoooooooooooooooooo

--- hola-mundo/src/pruebas/Principal.java
+++ hola-mundo/src/pruebas/Principal.java
@@ -4,6 +4,10 @@ public class Principal {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo");
+		System.out.println("bebesita");
+		System.out.println("ella no me ama");
+		System.out.println("top 5 makakos");
+		System.out.println("eseba carayo");
 	}
 
 }
